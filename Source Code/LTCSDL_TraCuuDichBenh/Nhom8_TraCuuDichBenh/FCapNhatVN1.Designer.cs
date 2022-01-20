d2ac1300cb17218fe0b328f86605b29b0feaff3b
1954052080quynh@ou.edu.vn <1954052080quynh@ou.edu.vn>
2022-01-20 10:50:14 +0700

    Vương - Cập nhật form CapNhatVN1

--- Source Code/LTCSDL_TraCuuDichBenh/Nhom8_TraCuuDichBenh/FCapNhatVN1.Designer.cs
+++ Source Code/LTCSDL_TraCuuDichBenh/Nhom8_TraCuuDichBenh/FCapNhatVN1.Designer.cs
@@ -46,6 +46,13 @@ namespace Nhom8_TraCuuDichBenh
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btQuayLai = new System.Windows.Forms.Button();
+            this.lbVNNhiemTong = new System.Windows.Forms.Label();
+            this.lbVNNhiemMoi = new System.Windows.Forms.Label();
+            this.lbVNTuTong = new System.Windows.Forms.Label();
+            this.lbVNTuMoi = new System.Windows.Forms.Label();
+            this.lbVNTiemMot = new System.Windows.Forms.Label();
+            this.lbVNTiemDu = new System.Windows.Forms.Label();
+            this.lbTenTp = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grdDsachVN1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -57,7 +64,7 @@ namespace Nhom8_TraCuuDichBenh
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(619, 210);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(204, 26);
+            this.label6.Size = new System.Drawing.Size(253, 31);
             this.label6.TabIndex = 11;
             this.label6.Text = "Thông tin Việt Nam:";
             // 
@@ -67,9 +74,10 @@ namespace Nhom8_TraCuuDichBenh
             this.btGiam.Name = "btGiam";
             this.btGiam.Size = new System.Drawing.Size(121, 45);
             this.btGiam.TabIndex = 9;
-            this.btGiam.Text = "Xóa";
+            this.btGiam.Text = "Giảm";
             this.toolTip1.SetToolTip(this.btGiam, "Dùng để xóa đi 1 lượng nhất định");
             this.btGiam.UseVisualStyleBackColor = true;
+            this.btGiam.Click += new System.EventHandler(this.btGiam_Click);
             // 
             // btThem
             // 
@@ -77,15 +85,16 @@ namespace Nhom8_TraCuuDichBenh
             this.btThem.Name = "btThem";
             this.btThem.Size = new System.Drawing.Size(121, 45);
             this.btThem.TabIndex = 8;
-            this.btThem.Text = "Cập nhật";
+            this.btThem.Text = "Thêm";
             this.toolTip1.SetToolTip(this.btThem, "Dùng để tăng số lượng");
             this.btThem.UseVisualStyleBackColor = true;
+            this.btThem.Click += new System.EventHandler(this.btThem_Click);
             // 
             // txtMui1
             // 
-            this.txtMui1.Location = new System.Drawing.Point(190, 110);
+            this.txtMui1.Location = new System.Drawing.Point(224, 106);
             this.txtMui1.Name = "txtMui1";
-            this.txtMui1.Size = new System.Drawing.Size(212, 32);
+            this.txtMui1.Size = new System.Drawing.Size(212, 38);
             this.txtMui1.TabIndex = 7;
             // 
             // label5
@@ -93,7 +102,7 @@ namespace Nhom8_TraCuuDichBenh
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(29, 113);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(71, 26);
+            this.label5.Size = new System.Drawing.Size(87, 31);
             this.label5.TabIndex = 6;
             this.label5.Text = "Mũi 1:";
             // 
@@ -103,15 +112,18 @@ namespace Nhom8_TraCuuDichBenh
             this.grdDsachVN1.Enabled = false;
             this.grdDsachVN1.Location = new System.Drawing.Point(624, 265);
             this.grdDsachVN1.Name = "grdDsachVN1";
+            this.grdDsachVN1.RowHeadersWidth = 51;
             this.grdDsachVN1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grdDsachVN1.Size = new System.Drawing.Size(510, 373);
+            this.grdDsachVN1.Size = new System.Drawing.Size(548, 373);
             this.grdDsachVN1.TabIndex = 10;
+            this.grdDsachVN1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdDsachVN1_CellClick);
+            this.grdDsachVN1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdDsachVN1_CellContentClick);
             // 
             // txtMui2
             // 
-            this.txtMui2.Location = new System.Drawing.Point(190, 168);
+            this.txtMui2.Location = new System.Drawing.Point(224, 164);
             this.txtMui2.Name = "txtMui2";
-            this.txtMui2.Size = new System.Drawing.Size(212, 32);
+            this.txtMui2.Size = new System.Drawing.Size(212, 38);
             this.txtMui2.TabIndex = 5;
             // 
             // label4
@@ -119,16 +131,16 @@ namespace Nhom8_TraCuuDichBenh
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(29, 171);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 26);
+            this.label4.Size = new System.Drawing.Size(87, 31);
             this.label4.TabIndex = 4;
             this.label4.Text = "Mũi 2:";
             // 
             // txtMaTP
             // 
             this.txtMaTP.Enabled = false;
-            this.txtMaTP.Location = new System.Drawing.Point(190, 52);
+            this.txtMaTP.Location = new System.Drawing.Point(224, 48);
             this.txtMaTP.Name = "txtMaTP";
-            this.txtMaTP.Size = new System.Drawing.Size(80, 32);
+            this.txtMaTP.Size = new System.Drawing.Size(80, 38);
             this.txtMaTP.TabIndex = 1;
             // 
             // label2
@@ -136,7 +148,7 @@ namespace Nhom8_TraCuuDichBenh
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(29, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(149, 26);
+            this.label2.Size = new System.Drawing.Size(185, 31);
             this.label2.TabIndex = 0;
             this.label2.Text = "Mã tỉnh thành:";
             // 
@@ -163,7 +175,7 @@ namespace Nhom8_TraCuuDichBenh
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(322, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(541, 46);
+            this.label1.Size = new System.Drawing.Size(672, 58);
             this.label1.TabIndex = 0;
             this.label1.Text = "CẬP NHẬT DIỄN BIẾN DỊCH";
             // 
@@ -205,12 +217,82 @@ namespace Nhom8_TraCuuDichBenh
             this.btQuayLai.UseVisualStyleBackColor = false;
             this.btQuayLai.Click += new System.EventHandler(this.btQuayLai_Click);
             // 
+            // lbVNNhiemTong
+            // 
+            this.lbVNNhiemTong.AutoSize = true;
+            this.lbVNNhiemTong.Location = new System.Drawing.Point(58, 155);
+            this.lbVNNhiemTong.Name = "lbVNNhiemTong";
+            this.lbVNNhiemTong.Size = new System.Drawing.Size(161, 31);
+            this.lbVNNhiemTong.TabIndex = 13;
+            this.lbVNNhiemTong.Text = "Nhiễm Tổng";
+            // 
+            // lbVNNhiemMoi
+            // 
+            this.lbVNNhiemMoi.AutoSize = true;
+            this.lbVNNhiemMoi.Location = new System.Drawing.Point(238, 155);
+            this.lbVNNhiemMoi.Name = "lbVNNhiemMoi";
+            this.lbVNNhiemMoi.Size = new System.Drawing.Size(142, 31);
+            this.lbVNNhiemMoi.TabIndex = 14;
+            this.lbVNNhiemMoi.Text = "Nhiễm Mới";
+            // 
+            // lbVNTuTong
+            // 
+            this.lbVNTuTong.AutoSize = true;
+            this.lbVNTuTong.Location = new System.Drawing.Point(400, 155);
+            this.lbVNTuTong.Name = "lbVNTuTong";
+            this.lbVNTuTong.Size = new System.Drawing.Size(115, 31);
+            this.lbVNTuTong.TabIndex = 15;
+            this.lbVNTuTong.Text = "Tử Tổng";
+            // 
+            // lbVNTuMoi
+            // 
+            this.lbVNTuMoi.AutoSize = true;
+            this.lbVNTuMoi.Location = new System.Drawing.Point(542, 155);
+            this.lbVNTuMoi.Name = "lbVNTuMoi";
+            this.lbVNTuMoi.Size = new System.Drawing.Size(96, 31);
+            this.lbVNTuMoi.TabIndex = 16;
+            this.lbVNTuMoi.Text = "Tử Mới";
+            // 
+            // lbVNTiemMot
+            // 
+            this.lbVNTiemMot.AutoSize = true;
+            this.lbVNTiemMot.Location = new System.Drawing.Point(644, 155);
+            this.lbVNTiemMot.Name = "lbVNTiemMot";
+            this.lbVNTiemMot.Size = new System.Drawing.Size(126, 31);
+            this.lbVNTiemMot.TabIndex = 17;
+            this.lbVNTiemMot.Text = "Tiêm Một";
+            // 
+            // lbVNTiemDu
+            // 
+            this.lbVNTiemDu.AutoSize = true;
+            this.lbVNTiemDu.Location = new System.Drawing.Point(787, 155);
+            this.lbVNTiemDu.Name = "lbVNTiemDu";
+            this.lbVNTiemDu.Size = new System.Drawing.Size(116, 31);
+            this.lbVNTiemDu.TabIndex = 18;
+            this.lbVNTiemDu.Text = "Tiêm Đủ";
+            // 
+            // lbTenTp
+            // 
+            this.lbTenTp.AutoSize = true;
+            this.lbTenTp.Location = new System.Drawing.Point(58, 586);
+            this.lbTenTp.Name = "lbTenTp";
+            this.lbTenTp.Size = new System.Drawing.Size(161, 31);
+            this.lbTenTp.TabIndex = 19;
+            this.lbTenTp.Text = "Nhiễm Tổng";
+            // 
             // FCapNhatVN1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
             this.ClientSize = new System.Drawing.Size(1184, 662);
+            this.Controls.Add(this.lbTenTp);
+            this.Controls.Add(this.lbVNTiemDu);
+            this.Controls.Add(this.lbVNTiemMot);
+            this.Controls.Add(this.lbVNTuMoi);
+            this.Controls.Add(this.lbVNTuTong);
+            this.Controls.Add(this.lbVNNhiemMoi);
+            this.Controls.Add(this.lbVNNhiemTong);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.grdDsachVN1);
@@ -252,5 +334,12 @@ namespace Nhom8_TraCuuDichBenh
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btQuayLai;
+        private System.Windows.Forms.Label lbVNNhiemTong;
+        private System.Windows.Forms.Label lbVNNhiemMoi;
+        private System.Windows.Forms.Label lbVNTuTong;
+        private System.Windows.Forms.Label lbVNTuMoi;
+        private System.Windows.Forms.Label lbVNTiemMot;
+        private System.Windows.Forms.Label lbVNTiemDu;
+        private System.Windows.Forms.Label lbTenTp;
     }
 }
--- Source Code/LTCSDL_TraCuuDichBenh/Nhom8_TraCuuDichBenh/FCapNhatVN1.cs
+++ Source Code/LTCSDL_TraCuuDichBenh/Nhom8_TraCuuDichBenh/FCapNhatVN1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using wfBLL;
 using wfDTO;
+using wfDAL;
+using System.Data.SqlClient;
 
 
 namespace Nhom8_TraCuuDichBenh
@@ -16,6 +18,13 @@ namespace Nhom8_TraCuuDichBenh
     public partial class FCapNhatVN1 : Form
     {
         VietNamBLL vn_bll = new VietNamBLL();
+        ConnectData dt = new ConnectData();
+
+        #region bien toan cuc
+        // String MaTp, TenTp;
+        double nhiemTong, nhiemMoi, tuTong, tuMoi, tiemMot, tiemDu; // VN: hang duoc chon
+        /*double VNNhiemTong, VNNhiemMoi, VNTuTong, VNTuMoi, VNKhoiTong, VNTiemMot, VNTiemDu;// VN: hang tong VN00*/
+        #endregion        
         public FCapNhatVN1()
         {
             InitializeComponent();
@@ -24,6 +33,22 @@ namespace Nhom8_TraCuuDichBenh
         private void FCapNhatVN1_Load(object sender, EventArgs e)
         {
             grdDsachVN1.DataSource = vn_bll.GetList_TiemChung();
+
+            /*#region Doc du lieu hàng VN00 từ bảng VN*/
+            DataTable dt1 = dt.GetData("select * from VietNam where MaTP = 'VN00'");
+            lbVNNhiemTong.DataBindings.Clear();
+            lbVNNhiemTong.DataBindings.Add("Text", dt1, "NhiemTong", true);
+            lbVNNhiemMoi.DataBindings.Clear();
+            lbVNNhiemMoi.DataBindings.Add("Text", dt1, "NhiemMoi", true);
+            lbVNTuTong.DataBindings.Clear();
+            lbVNTuTong.DataBindings.Add("Text", dt1, "TuTong", true);
+            lbVNTuMoi.DataBindings.Clear();
+            lbVNTuMoi.DataBindings.Add("Text", dt1, "TuMoi", true);
+            lbVNTiemMot.DataBindings.Clear();
+            lbVNTiemMot.DataBindings.Add("Text", dt1, "TiemMot", true);
+            lbVNTiemDu.DataBindings.Clear();
+            lbVNTiemDu.DataBindings.Add("Text", dt1, "TiemDu", true);
+            /*#endregion*/
         }
 
         #region Các button chuyển form
@@ -36,5 +61,98 @@ namespace Nhom8_TraCuuDichBenh
         }
         #endregion
 
+        private void grdDsachVN1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.RowIndex >= 0)
+
+            {
+                DataGridViewRow row = this.grdDsachVN1.Rows[e.RowIndex];
+                txtMaTP.Text = row.Cells[0].Value.ToString();
+                lbTenTp.Text = row.Cells[1].Value.ToString();
+                if (txtMaTP.Text == "VN32")
+                    MessageBox.Show("Bạn không thể cập nhật tại bảng này!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    nhiemTong = int.Parse(row.Cells[2].Value.ToString());
+                    nhiemMoi = int.Parse(row.Cells[3].Value.ToString());
+                    tuTong = int.Parse(row.Cells[4].Value.ToString());
+                    tuMoi = int.Parse(row.Cells[5].Value.ToString());
+                    tiemMot = int.Parse(row.Cells[6].Value.ToString());
+                    tiemDu = int.Parse(row.Cells[7].Value.ToString());
+                }
+
+                if (txtMaTP.Text == "QG00")
+                    MessageBox.Show("Bạn không thể cập nhật trường này!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            /*// lay du lieu            
+            tiemMot = int.Parse(txtMui1.Text);
+            tiemDu = int.Parse(txtMui2.Text);
+            //lay du lieu hang VN00
+            VNTiemMot = int.Parse(lbTiemMot.Text);
+            VNTiemDu = int.Parse(lbTiemDu.Text);
+
+
+            // hang chon
+            tiemMot =+ tiemMot;
+
+            // hang tong VN00
+            VNTiemMot += tiemMot;
+            VNTiemDu += tiemDu;
+
+
+            //luu hang chon
+            VietNamDTO _tp = new VietNamDTO();
+            _tp.MaTp = txtMaTP.Text;
+            _tp.TiemMot = tiemMot;
+            _tp.TiemDu = tiemDu;
+            vn_bll.CapNhat(_tp);
+
+            // Luu vao VN00
+            VietNamDTO _tp1 = new VietNamDTO();
+            _tp1.MaTp = "VN00";
+            _tp1.TiemMot = VNTiemMot;
+            _tp1.TiemDu = VNTiemDu;
+            vn_bll.CapNhat(_tp1);
+            FCapNhatVN1_Load(sender, e);*/
+        }
+
+        private void btGiam_Click(object sender, EventArgs e)
+        {
+            /*// lay du lieu 
+
+            tiemMot = int.Parse(txtMui1.Text);
+            tiemDu = int.Parse(txtMui2.Text);
+
+            //lay du lieu hang VN00
+            VNTiemMot = int.Parse(lbTiemMot.Text);
+            VNTiemDu = int.Parse(lbTiemDu.Text);
+
+            // hang chon
+            tiemMot -= tiemMot;
+            tiemDu -= tiemDu;
+
+            // hang tong VN00
+            VNTiemMot -= tiemMot;
+            VNTiemDu -= tiemDu;
+
+            // luu hang chon
+            VietNamDTO _tp = new VietNamDTO();
+            _tp.MaTp = txtMaTP.Text;
+            _tp.TiemMot = tiemMot;
+            _tp.TiemDu = tiemDu;
+            vn_bll.CapNhat(_tp);
+            // Luu vao QG00
+            VietNamDTO _tp1 = new VietNamDTO();
+            _tp1.TiemMot = VNTiemMot;
+            _tp1.TiemDu = VNTiemDu;
+            FCapNhatVN1_Load(sender, e);*/
+        }
+        private void grdDsachVN1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
     }
 }
