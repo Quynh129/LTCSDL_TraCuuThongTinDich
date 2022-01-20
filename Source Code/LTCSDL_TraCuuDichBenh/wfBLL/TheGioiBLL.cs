6d9672c939428957d2f7fa92e24aecbe1eb75093
1954052080quynh@ou.edu.vn <1954052080quynh@ou.edu.vn>
2022-01-20 12:35:11 +0700

    Trúc_ cập nhật form CapNhatTG

--- Source Code/LTCSDL_TraCuuDichBenh/wfBLL/TheGioiBLL.cs
+++ Source Code/LTCSDL_TraCuuDichBenh/wfBLL/TheGioiBLL.cs
@@ -20,5 +20,16 @@ namespace wfBLL
         {
             return TG_dal.loadTTTG();
         }
+        public DataTable GetListUp()
+        {
+            return TG_dal.loadThongTinTG_Up();
+        }
+        public bool CapNhat(TheGioiDTO _tg)
+        {
+            if (string.IsNullOrEmpty(_tg.MaQg))
+                return false;
+            TG_dal.Update(_tg);
+            return true;
+        }
     }
 }
